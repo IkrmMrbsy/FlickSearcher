ab53728ccbe05f617e67c9db23866b0cec12c54a
ikram <ikrammarbasy17@gmail.com>
2025-03-23 17:24:43 +0700

    Add fetchTrendingMovies to retrieve trending movies with filtering

--- src/domain/useCases/fetchMovies.js
+++ src/domain/useCases/fetchMovies.js
@@ -10,6 +10,22 @@ export const fetchGenres = async (api) => {
     return genreList;
 };
 
+export const fetchTrendingMovies = async (api) => {
+    const response = await api.get('/trending/movie/week');
+    const genres = await fetchGenres(api);
+
+    return response.data.results
+        .filter(movie => 
+            movie.backdrop_path && 
+            movie.overview && 
+            movie.vote_average
+        )
+        .map(movie => {
+            const movieGenres = movie.genre_ids.map(id => genres.find(genre => genre.id === id)).filter(Boolean);
+            return new Movie({ ...movie, genres: movieGenres });
+        });
+};
+
 export const fetchMovies = async (api) => {
     const response = await api.get('/movie/popular');
     const genres = await fetchGenres(api);
