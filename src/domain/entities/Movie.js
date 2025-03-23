e46fe609c01b74d1e2e73697d6d8b4c89cd4c907
ikram <ikrammarbasy17@gmail.com>
2025-03-23 17:23:58 +0700

    Enhance Movie class by adding backdrop_path and tagline properties

--- src/domain/entities/Movie.js
+++ src/domain/entities/Movie.js
@@ -1,11 +1,13 @@
 export default class Movie {
-    constructor({id, title, poster_path, vote_average, release_date, genres, overview}) {
+    constructor({id, title, poster_path, backdrop_path, vote_average, release_date, genres, overview, tagline}) {
         this.id = id;
         this.title = title;
         this.poster_path = poster_path;
+        this.backdrop_path = backdrop_path;
         this.vote_average = vote_average;
         this.release_date = release_date;
         this.genres = genres;
         this.overview = overview;
+        this.tagline = tagline;
     }
 }
